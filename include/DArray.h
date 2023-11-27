9f3e1e2c0de4668902752eae1666f1420f550de5
Qervas <djmax96945147@outlook.com>
2023-11-27 06:11:55 +0100

    Refactor DArray to Use CudaMemory for Enhanced Safety
    This commit introduces a significant refactor of the DArray class, integrating the CudaMemory class for GPU memory management. The primary goal of this change is to enhance the safety, robustness, and maintainability of the DArray class, particularly in handling GPU resources.
    
    Key Changes:
    - Replaced `std::shared_ptr<T>` with `CudaMemory<T>` for managing the dynamic array on the GPU.
    - Updated the DArray constructor to leverage the CudaMemory class, simplifying the memory allocation and deallocation process.
    - Modified the `addr` and `clear` methods of DArray to work seamlessly with the CudaMemory class.
    - Ensured that the class adheres to the RAII (Resource Acquisition Is Initialization) principle, which is critical in managing resources in C++ effectively.
    
    Benefits:
    - Improved Error Handling: The CudaMemory class provides better handling of potential CUDA memory allocation failures.
    - Automatic Resource Management: By using RAII, GPU memory is automatically managed, reducing the risk of memory leaks and ensuring proper cleanup.
    - Increased Code Clarity: The refactor enhances the readability and maintainability of the DArray class by abstracting away the low-level details of memory management.
    
    Note:
    - This change requires the inclusion of the CudaMemory class in the project.
    - The static assertions and other functionalities of DArray remain unchanged.

--- include/DArray.h
+++ include/DArray.h
@@ -1,38 +1,38 @@
-
-
 #pragma once
 #include "global.h"
 #include <cuda_runtime.h>
+#include "CudaMemory.h"  // Include the CudaMemory header
+
 template <typename T>
 class DArray {
-	static_assert(
-		std::is_same<T, float3>::value || std::is_same<T, float>::value || 
-		std::is_same<T, int>::value, "DataArray must be of int, float or float3.");
+    static_assert(
+        std::is_same<T, float3>::value || std::is_same<T, float>::value || 
+        std::is_same<T, int>::value, "DArray must be of int, float, or float3.");
+
 public:
-	explicit DArray(const unsigned int length) :
-		_length(length),
-		d_array([length]() {
-			T* ptr; 
-			checkCudaErrors(cudaMalloc((void**)& ptr, sizeof(T) * length));
-			std::shared_ptr<T> t(new(ptr)T[length], [](T* ptr) {checkCudaErrors(cudaFree(ptr)); });
-			return t;
-		}()) {
-			this->clear();
-	}
+    explicit DArray(unsigned int length) :
+        _length(length),
+        d_array(length) {
+        this->clear();
+    }
 
-	DArray(const DArray&) = delete;
-	DArray& operator=(const DArray&) = delete;
+    DArray(const DArray&) = delete;
+    DArray& operator=(const DArray&) = delete;
 
-	T* addr(const int offset= 0) const {
-		return d_array.get() + offset;
-	}
+    T* addr(int offset = 0) const {
+        return d_array.get() + offset;
+    }
 
-	unsigned int length() const { return _length; }
-	void clear(){ checkCudaErrors(cudaMemset(this->addr(), 0, sizeof(T) * this->length())); }
-	
-	~DArray() noexcept { }
+    unsigned int length() const { return _length; }
+    void clear() { 
+        if (_length > 0) {
+            checkCudaErrors(cudaMemset(this->addr(), 0, sizeof(T) * _length)); 
+        }
+    }
+
+    ~DArray() noexcept { }
 
 private:
-	const unsigned int _length;
-	const std::shared_ptr<T> d_array;
+    const unsigned int _length;
+    CudaMemory<T> d_array;  // Replace std::shared_ptr with CudaMemory
 };
